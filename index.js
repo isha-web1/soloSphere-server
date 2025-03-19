8d748f2101dc1b3d8a46d898eed4d136fc9eba2d
isha-web1 <isha.web1@gmail.com>
2025-03-19 23:23:24 +0600

    server initial done

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+const app = express()
+
+const corsOptions = {
+    origin: [
+      'http://localhost:5173',
+      
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+
+  app.use(cors(corsOptions))
+  app.use(express.json())
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello from SoloSphere Server....')
+  })
+  
+  app.listen(port, () => console.log(`Server running on port ${port}`))
